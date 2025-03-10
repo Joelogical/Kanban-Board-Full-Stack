2a6f7df118c7117020e684693539d95a4796349a
Joel S <jmsmith.coding@gmail.com>
2025-03-10 01:24:12 -0700

    updated index.ts, more console logs

--- Develop/server/src/models/index.ts
+++ Develop/server/src/models/index.ts
@@ -1,12 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log("Starting model initialization...");
+
 import { Sequelize } from "sequelize";
 import { UserFactory } from "./user.js";
 import { TicketFactory } from "./ticket.js";
 import sequelize from "../config/connection.js";
 
+console.log("About to initialize User model...");
 const User = UserFactory(sequelize);
+console.log("About to initialize Ticket model...");
 const Ticket = TicketFactory(sequelize);
 
 console.log("Models initialized:", {
@@ -14,7 +18,9 @@ console.log("Models initialized:", {
   Ticket: !!Ticket,
 });
 
+console.log("Setting up associations...");
 User.hasMany(Ticket, { foreignKey: "assignedUserId" });
 Ticket.belongsTo(User, { foreignKey: "assignedUserId", as: "assignedUser" });
+console.log("Associations complete");
 
 export { sequelize, User, Ticket };
