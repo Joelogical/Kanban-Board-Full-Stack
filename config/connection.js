025d185a2586f149ffd60c682afdaad81c77db3d
Joel S <jmsmith.coding@gmail.com>
2025-03-09 22:50:34 -0700

    udated pgadmin_db, updated config folder

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "postgres",
+        port: 5432,
+      }
+    );
