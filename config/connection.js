3d0639801880aecd4019b6a686ab010b6eb7ec41
Joel S <jmsmith.coding@gmail.com>
2025-03-09 23:10:10 -0700

    updated connections.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,16 @@
 const Sequelize = require("sequelize");
 
-// Remove dotenv config since Render provides env vars directly
+// Debug environment
 console.log("Environment check:");
 console.log("Current directory:", process.cwd());
+console.log("All environment variables:", Object.keys(process.env));
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
 console.log("Is DATABASE_URL defined?", !!process.env.DATABASE_URL);
+
+// Ensure DATABASE_URL exists
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
 
 // Force SSL for Render
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
@@ -15,6 +21,17 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
   },
   dialect: "postgres",
+  logging: console.log, // Enable query logging
 });
+
+// Test the connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to database:", err);
+  });
 
 module.exports = sequelize;
