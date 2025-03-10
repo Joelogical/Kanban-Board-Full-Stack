c48103e0946671b5da874334b4de8d49b2b1720d
Joel S <jmsmith.coding@gmail.com>
2025-03-09 23:02:06 -0700

    updated connections.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-require("dotenv").config({ path: "../Develop/server/.env" });
+require("dotenv").config({ path: "./Develop/server/.env" });
 
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
