d0012e834adef010d0180587e07c311dc6ac2580
Joel S <jmsmith.coding@gmail.com>
2025-03-10 01:33:26 -0700

    updated index.ts, server.ts, ticket-routes.ts

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -24,7 +24,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // API Routes
+console.log("Mounting API routes...");
 app.use("/api", apiRoutes);
+console.log("API routes mounted");
 
 // Debug route to test ticket controller directly
 app.get("/test-tickets", async (req, res) => {
